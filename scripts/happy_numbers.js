67482a43d3c18b9d2bdcb4bd019a9ee91c736d0f
lawrencetheabhorrence <tripwireplease@gmail.com>
2020-09-20 11:38:55 +0800

    did some changes to happy numbers but it's not working honestly i hate it so much lol

--- scripts/happy_numbers.js
+++ scripts/happy_numbers.js
@@ -1,37 +1,27 @@
-function isHappyNumber(n, k, count){
-	if (k == 1) return true;
-	if (n == k && count > 0) return false;
-	return isHappyNumber(n, sumOfDigitsSquared(k), count++);
-}
-
-out = [1];
-
-function happyNumberSeq(n, k, count){
-	if (!isHappyNumber(n)) return [];
-	if(k == 1) return out;
-	else{
-		out.push(k);
-		happyNumberSeq(n, sumOfDigitsSquared(k), count++);
-	}
-}
-
 function sumOfDigitsSquared(n){
-	n = n.toString.split('');
 	out = 0;
-	for(x = 0; x < n.length(); x++) out += Integer.parseInt(n) * (10 ^ (n.length() - x));
+	while (n > 0){
+		out += pow(n % 10, 2);
+		n = Math.floor(n / 10);
+	}
+	return out;
+}
+
+function isHappy(x){
+	seennumbers = [];
+	while ((x > 1) && (seennumbers.indexOf(x) == -1)){
+		seennumbers.push(x);
+		x = sumOfDigitsSquared(x);
+	}
+	return (number == 1);
 }
 
 function happy_button(){
-	happy_numbers = [];
-	n = document.getElementById("n_happy_input").value;
-	for(x = 1; x <= n; x++){
-		if(happy_numbers.length() == n) break;
-		if(happy_numbers.indexOf(x) != -1) continue;
-		if(isHappyNumber(x)){
-			happy_numbers.concat(happyNumberSeq);
-		}
-	}
-
+	n = parseInt(document.getElementById("n_happy_input").value);
 	out = document.getElementById("happy_output");
-	out.value = "The first " + n + " happy numbers are " + happy_numbers.out;
+	happies = "";
+	for(x = 1; x <= n; x++){
+		if(isHappy(x)) happies += x + " ";
+	}
+	out.value = happies;
 }
