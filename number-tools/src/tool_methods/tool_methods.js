4064d82e3a9c84728e054a69fbf4ade7101f8a16
lawrencetheabhorrence <tripwireplease@gmail.com>
2020-11-28 22:32:14 +0800

    added more method to the tool_methods

--- number-tools/src/tool_methods/tool_methods.js
+++ number-tools/src/tool_methods/tool_methods.js
@@ -21,6 +21,8 @@ const fibonacciUpTo = n => {
   }
 }
 
+// FACTORIAL FUNCTIONS
+
 const factorialLoop = n => {
   if (n > -1) {
     if (n === 0 || n === 1) return 1
@@ -39,10 +41,41 @@ const factorialRecursion = n => {
   return n * factorialRecursion(n - 1)
 }
 
+// PRIME FACTORIZATION AND GENERATION FUNCTIONS
+
 const primeFactorization = n => {
   let factors = primeFactorHelper.listOfFactors(n)
   factors.map(f => `+${primeFactorHelper.powerOfFactor(n, f)}+`)
   return factors.join
 }
 
-export default { fibonacciUpTo, factorialLoop, factorialRecursion, primeFactorization }
+const primeGen => {
+  // generates primes through the euclidean method
+  let i = 1
+  let primes = [2]
+  for (x of primes)
+    i *= x
+  i++
+  primes.push(i)
+}
+
+// BINARY - DEC CONVERSION
+
+const confirmBinary = n => n.match(/[^0-1]+/) === null
+
+const binToDec = n => {
+  // splits the digits into bits
+  // the reducer simply multiplies each bit by 2 raised to their index
+  // note: the bits are evaluated left to right
+  const reducer = (accum, val, i, digits) => Math.pow(2, digits.length - 1 - i)*parseInt(val)
+  return confirmBinary(n) ? n.split('').reduce(reducer) : 'Not Binary'
+}
+
+const dtbHelper = (n, out) =>
+  // helper function for the decToBin function
+  if (n == 0) return out.split('').reverse().join('') // reverses the binary bits
+  return (n % 2 === 1) ? dtbHelper((n-1) / 2, out.concat('1')) : dtbHelper(n / 2, out.concat('0')) // determines binary bits (tho in reverse) ??
+
+const decToBin = (n) => dtbHelper(n, "")
+
+export default { fibonacciUpTo, factorialLoop, factorialRecursion, primeFactorization, primeGen, binToDec, decToBin}
