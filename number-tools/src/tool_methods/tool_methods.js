33d08c0d1c3647e94ae8c2e78ac6beab6af82aa5
EVC Chronicles Admin <tripwireplease@gmail.com>
2020-12-02 13:09:31 +0800

    added numberNames to tool_methods

--- number-tools/src/tool_methods/tool_methods.js
+++ number-tools/src/tool_methods/tool_methods.js
@@ -71,10 +71,11 @@ const binToDec = n => {
   return confirmBinary(n) ? n.split('').reduce(reducer) : 'Not Binary'
 }
 
-const dtbHelper = (n, out) =>
+const dtbHelper = (n, out) => {
   // helper function for the decToBin function
   if (n === 0) return out.split('').reverse().join('') // reverses the binary bits
   return (n % 2 === 1) ? dtbHelper((n-1) / 2, out.concat('1')) : dtbHelper(n / 2, out.concat('0')) // determines binary bits (tho in reverse) ??
+}
 
 const decToBin = (n) => dtbHelper(n, "")
 
@@ -92,4 +93,45 @@ const fastExpo = (n, k) => {
   return (k % 2 === 0) ? n * n * fastExpo(n, k - 2) : n * fast_expo(n, k - 1)
 }
 
-export default { fibonacciUpTo, factorialLoop, factorialRecursion, primeFactorization, primeGen, binToDec, decToBin, changeDenom, fastExpo}
+const numberNameChunk = n => {
+  // names numbers up to 999
+
+  if (n == 0) return ""
+
+  const ones = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+  const tens = ["twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+  const tenSet = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+  let out = ""
+  let divisor = 1
+
+  while (Math.floor(n / divisor) != 0) {
+    digit = Math.floor(n / divisor) % 10
+
+    if (divisor == 1 && digit > 0) out = ones[digit - 1] + out // ones place
+    if (divisor == 10) {
+      if (digit == 1)  out = tenSet[n % 10] // if tens place is 1, return the number name for the tens
+      if (digit > 2) out = tens[digit - 2] + " " + out
+    }
+    if (divisor == 100) out = ones[digit - 1] + " hundred " + out // hundreds place
+
+    divisor *= 10
+  }
+
+  return out
+}
+
+numberNames = n => {
+  // handles input up to 1 million
+  if (n > Math.pow(10, 6)) return "enter a smaller number"
+  if (n < 0) return "negative numbers not yet supported"
+  if (n == Math.pow(10, 6)) return "one million"
+  if (n == 0) return "zero"
+
+  const firstChunk = n % 1000
+  const secondChunk = Math.floor(n / 1000)
+  if (secondChunk > 0) return numberNameChunk(secondChunk) + " thousand " + numberNameChunk(firstChunk)
+
+  return numberNameChunk(firstChunk)
+}
+
+export default { fibonacciUpTo, factorialLoop, factorialRecursion, primeFactorization, primeGen, binToDec, decToBin, changeDenom, fastExpo, numberNames }
