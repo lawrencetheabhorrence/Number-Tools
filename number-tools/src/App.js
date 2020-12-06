720f9e99a04c7c0a137032052335f85979f0a4ab
EVC Chronicles Admin <tripwireplease@gmail.com>
2020-12-06 21:54:53 +0800

    added tools to the main app

--- number-tools/src/App.js
+++ number-tools/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import './index.css'
 import FibSeq from 'components/FibSeq'
+import Factorial from 'components/Factorial'
+import PrimeFactor from 'components/PrimeFactor'
 
 const App = () => {
   return (
     <div id='main'>
       <FibSeq />
+      <Factorial />
+      <PrimeFactor />
     </div>
   )
 }
